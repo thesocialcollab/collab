caf85915a1be2c471aa7f23aeecee4cfe0260b1d
Isaiah Young <contactisaiahyoung@gmail.com>
2023-10-11 22:40:15 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouterRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
 import { auth } from './firebase';
