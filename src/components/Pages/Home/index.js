1d4af69d72f7922acf74311127baa43e69b6d3e7
Isaiah Young <contactisaiahyoung@gmail.com>
2023-10-13 15:07:08 -0700

    Added home screen buttons for posts

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -16,47 +16,64 @@ import './index.css';
 const Home = () => {
     const [posts, setPosts] = useState([]);
     const [likedPosts, setLikedPosts] = useState([]);
+    const [selectedSort, setSelectedSort] = useState('latest');
     const db = getFirestore();
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            // Previous fetching posts logic
-            const qPosts = query(collection(db, "posts"), orderBy("timestamp", "desc"), limit(10));
-            const querySnapshotPosts = await getDocs(qPosts);
-            const postData = [];
-            querySnapshotPosts.forEach((doc) => {
-                postData.push({...doc.data(), id: doc.id}); // Now includes ID
-            });
-            setPosts(postData);
-        };
+    const fetchPosts = async (type) => {
+        let qPosts;
 
-        const fetchLikedPosts = async () => {
-            if (auth.currentUser) {
-                const userId = auth.currentUser.uid;
-                const qLikes = query(collection(db, "users", userId, "likes"));
-                const querySnapshotLikes = await getDocs(qLikes);
-                const likedData = [];
-                querySnapshotLikes.forEach((doc) => {
-                    likedData.push(doc.data().postId);
-                });
-                setLikedPosts(likedData);
-            }
-        };
+        switch (type) {
+            case 'hot':
+                qPosts = query(collection(db, "posts"), orderBy("likes", "desc"), limit(10));
+                break;
+            case 'newest':
+                qPosts = query(collection(db, "posts"), orderBy("timestamp", "desc"), limit(10));
+                break;
+            case 'latest':
+            default:
+                qPosts = query(collection(db, "posts"));
+                break;
+        }
 
-        fetchPosts().then(() => {
-            fetchLikedPosts();
+        const querySnapshotPosts = await getDocs(qPosts);
+        let postData = [];
+        querySnapshotPosts.forEach((doc) => {
+            postData.push({ ...doc.data(), id: doc.id });
         });
-    }, [db]);
 
+        if (type === 'latest') {
+            postData = postData.sort(() => Math.random() - 0.5);
+        }
 
-    
+        setPosts(postData);
+    };
+
+    const fetchLikedPosts = async () => {
+        if (auth.currentUser) {
+            const userId = auth.currentUser.uid;
+            const qLikes = query(collection(db, "users", userId, "likes"));
+            const querySnapshotLikes = await getDocs(qLikes);
+            const likedData = [];
+            querySnapshotLikes.forEach((doc) => {
+                likedData.push(doc.data().postId);
+            });
+            setLikedPosts(likedData);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts(selectedSort).then(fetchLikedPosts);
+    }, [db, selectedSort]);
+
     const toggleLike = async (postId) => {
         if (!auth.currentUser) {
             console.error("User not authenticated!");
             return;
         }
 
+
         const userId = auth.currentUser.uid;
+
 
         if (likedPosts.includes(postId)) {
             // UNLIKE
@@ -68,9 +85,11 @@ const Home = () => {
                     await deleteDoc(docSnapshot.ref);  // <--- This line is adjusted
                 });
 
+
                 // Update local state
                 setLikedPosts(prevLiked => prevLiked.filter(id => id !== postId));
                 console.log("Post successfully unliked!");
+
 
             } catch (error) {
                 console.error("Error unliking post:", error);
@@ -84,20 +103,26 @@ const Home = () => {
                     timestamp: new Date()
                 });
 
+
                 // Update local state
                 setLikedPosts(prevLiked => [...prevLiked, postId]);
                 console.log("Post successfully liked!");
+
 
             } catch (error) {
                 console.error("Error liking post:", error);
             }
         }
     };
-    
+
 
     return (
         <div className="home-container">
-            <h1>Latest Posts</h1>
+            <div className="sorting-buttons">
+                <button onClick={() => setSelectedSort('recommended')} className={selectedSort === 'recommended' ? 'active' : ''}>Recommended</button>
+                <button onClick={() => setSelectedSort('hot')} className={selectedSort === 'hot' ? 'active' : ''}>Hot</button>
+                <button onClick={() => setSelectedSort('latest')} className={selectedSort === 'latest' ? 'active' : ''}>Latest</button>
+            </div>
             {posts.map((post) => (
                 <div key={post.id} className="post-container-item">
                     <h3>{post.username}</h3>
